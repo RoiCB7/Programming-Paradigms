2d808cf05009f2940746c1a55b72f3d31d891ab9
RoiCB7 <16857308+RoiCB7@users.noreply.github.com>
2021-02-01 15:40:34 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		try {
+		File textFile = new File (args[0]); 
+		Scanner readText = new Scanner (textFile);
+		System.out.println("Success!");
+		}
+		
+		catch (FileNotFoundException fnfe) {
+			System.err.println("Cannot open file");
+		}
+	}
+	
+	public static void textProcess (Scanner readText) {
+		
+		
 	}
 
 }

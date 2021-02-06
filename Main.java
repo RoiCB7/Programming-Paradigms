e62fe6bb2539ea8e672f90c11c1f6f67ad675e56
Josh Lewis <josh.rb.lewis@gmail.com>
2021-02-05 19:58:49 -0700

    test

--- Main.java
+++ Main.java
@@ -7,32 +7,33 @@ public class Main {
 	static ArrayList<Character> listOfChar = new ArrayList<Character>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+	    //this is a change
+
 		try {
-		File InputFile = new File (args[0]); 
+		File InputFile = new File (args[0]);
 		Scanner readFile = new Scanner (InputFile);
 		System.out.println("Success!");
 		textProcess(readFile);
 		}
-		
+
 		catch (FileNotFoundException fnfe) {
 			System.err.println("Cannot open file");
 		}
 	}
-	
+
 	public static void textProcess (Scanner readFile) {
-	
+
 		String line = readFile.nextLine();
 		char c;
 		for (int i = 0; i < line.length(); i++) {
 			c = line.charAt(i);
-			
+
 			if (c == '(') {
 				System.out.println("Found One");
 			}
-			
+
 		}
-		
+
 	}
 
 }

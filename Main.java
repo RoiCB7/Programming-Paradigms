31d9eda1194f1af395526a8f7e4a0ccb09371200
RoiCB7 <16857308+RoiCB7@users.noreply.github.com>
2021-02-05 19:31:21 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+	static ArrayList<Character> listOfChar = new ArrayList<Character>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
 		try {
-		File textFile = new File (args[0]); 
-		Scanner readText = new Scanner (textFile);
+		File InputFile = new File (args[0]); 
+		Scanner readFile = new Scanner (InputFile);
 		System.out.println("Success!");
-		textProcess (readText);
+		textProcess(readFile);
 		}
 		
 		catch (FileNotFoundException fnfe) {
@@ -19,8 +20,18 @@ public class Main {
 		}
 	}
 	
-	public static void textProcess (Scanner readText) {
-		//This is where we can parse the text file.
+	public static void textProcess (Scanner readFile) {
+	
+		String line = readFile.nextLine();
+		char c;
+		for (int i = 0; i < line.length(); i++) {
+			c = line.charAt(i);
+			
+			if (c == '(') {
+				System.out.println("Found One");
+			}
+			
+		}
 		
 	}
 
